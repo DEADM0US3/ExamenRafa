0a17a4d31d038f8bbe0728a3037675b2502e1a08
Arbey DzIb <120668243+Joestarb@users.noreply.github.com>
2023-10-20 09:01:35 -0500

    app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+function App() {
+  // axios.get('')
+  // .then(response => {
+  //   // Manejo de la respuesta exitosa
+  //   console.log(response.data);
+  // })
+  // .catch(error => {
+  //   // Manejo de errores
+  //   console.error(error);
+  // });
+
+  return (
+    <div>
+      <h1 className=" text-9xl">Hello World</h1>
+
+
+    </div>
+  );
+}
+
+export default App;
