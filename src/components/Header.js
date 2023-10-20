aafd81e7e5adf3dd3d6cd55f474d912974196c7d
Arbey DzIb <120668243+Joestarb@users.noreply.github.com>
2023-10-20 10:39:30 -0500

    hero image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import bgplant from "../assets/bg-plant.png";
-import plant from "../assets/plant.png";
 function Header() {
     return (
-        <div className='grid place-content-center mt-5    mx-20 rounded-3xl bg-[#C1DCDC]'>
+        <div className='grid place-content-center mt-5   hero mx-20 rounded-3xl '>
             <div className='  p-2'>
-                <div className='grid grid-cols-2'>
-                    <div>
-                        <h1 className='text-6xl font-bold mt-[10%]'>
-                            Busca la mejor planta para ti
+                <div className='grid '>
+                    <div className='px-10 w-full  bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg'>
+                        <h1 className='text-6xl font-bold '>
+                            Busca la mejor planta 
                         </h1>
-                        <p className='text-3xl'>+100</p>
-                        <p className='text-3xl'>plantas para que puedas ver</p>
+                        <p className='text-3xl text-center mt-6'>+100</p>
+                        <p className='text-3xl text-center'>plantas para que puedas ver</p>
                     </div>
-                    <div >
-                        <img className=' absolute z-20   ' src={plant} alt='plant' />
-                        <img className=' z-10 ' src={bgplant} alt='bgplant' />
-                    </div>
+                   
                 </div>
             </div>
         </div>
